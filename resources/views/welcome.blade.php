23d0b7be051bef9dfd2f043a21811158b70b43b7
iftakharul-islam <iftakharulislam881@gmail.com>
2021-11-09 02:38:45 -0800

    Project Init

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,152 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Chhagalnaiya Govt Collage</title>
+  </head>
+  <body>
+      <!-- Header -->
+    <div class="container">
+    <header >
+        <nav class="navbar navbar-expand-lg navbar-light" aria-label="Fifth navbar example">
+        <div class="container-fluid">
+       
+        <button class="navbar-toggler navbar-toggler-right collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#navbarsExample05" aria-controls="navbarsExample05" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="navbar-collapse collapse justify-content-end" id="navbarsExample05" style="">
+            <ul class="navbar-nav   mb-2 mb-lg-0">
+            <li class="nav-item">
+                <a class="nav-link active" aria-current="page" href="#">Home</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link" href="#">About Us</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link disabled">Admision</a>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="administration" data-bs-toggle="dropdown" aria-expanded="false">Administration</a>
+                <ul class="dropdown-menu" aria-labelledby="administration">
+                <li><a class="dropdown-item" href="#">Principle</a></li>
+                <li><a class="dropdown-item" href="#">Vice-Principle</a></li>
+                </ul>
+            </li>
+            <li class="nav-item dropdown">
+                <a class="nav-link dropdown-toggle" href="#" id="administration" data-bs-toggle="dropdown" aria-expanded="false">Teachers Info</a>
+                <ul class="dropdown-menu" aria-labelledby="administration">
+                <li><a class="dropdown-item" href="#">Bangla</a></li>
+                <li><a class="dropdown-item" href="#">English</a></li>
+                <li><a class="dropdown-item" href="#">Chemistry</a></li>
+                <li><a class="dropdown-item" href="#">Economics</a></li>
+                <li><a class="dropdown-item" href="#">Mathemetics</a></li>
+                <li><a class="dropdown-item" href="#">History</a></li>
+                <li><a class="dropdown-item" href="#">Philospy</a></li>
+                </ul>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">Notice</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">Results</a>
+            </li>
+            <!-- <li class="nav-item">
+                <a class="nav-link">Video Gellary</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">Photo Gellary</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link">Online Class</a> -->
+            </li>
+            </ul>
+        </div>
+        </div>
+    </nav>
+    </header>
+  </div>
+
+  <!-- Hero Section -->
+  <div class="container col-xxl-8 px-4">
+    <div class="row flex-lg-row-reverse align-items-center g-5 py-5">
+      <div class="col-10 col-sm-8 col-lg-6">
+        <img src="{{asset('assets/img/logo.jfif')}}" class="d-block mx-lg-auto img-fluid" alt="Bootstrap Themes" width="700" height="500" loading="lazy">
+      </div>
+      <div class="col-lg-6">
+        <h1 class="display-5 fw-bold lh-1 mb-3">ছাগলনাইয়া সরকারি কলেজ</h1>
+        <p class="lead">ছাগলনাইয়া সরকারি কলেজ বাংলাদেশের ফেনী জেলা ছাগলনাইয়া উপজেলার একটি পুরোনো ও ঐতিহ্যবাহী শিক্ষাপ্রতিষ্ঠান। এ কলেজটি ১৯৭২ সালের প্রতিষ্ঠিত হয়।</p>
+        <p class="lead">১৯৮৬ সালে কলেজটি ডিগ্রি পর্যায়ে উন্নীত হয় এবং ১৯৮৮ সালে সরকারীকরণ করা হয়। কয়েক দশক এ কলেজটি উচ্চশিক্ষার ক্ষেত্রে গুরুত্বপূর্ণ ভূমিকা রেখেছে</p>
+        
+      </div>
+    </div>
+  </div>
+
+
+  <!-- Footer -->
+  <div class="container">
+  <footer class="row row-cols-5 py-5 my-5 border-top">
+    <div class="col">
+      <a href="/" class="d-flex align-items-center mb-3 link-dark text-decoration-none">
+        <svg class="bi me-2" width="40" height="32"><use xlink:href="#bootstrap"></use></svg>
+      </a>
+      <p class="text-muted">© 2021</p>
+    </div>
+
+    <div class="col">
+
+    </div>
+
+    <div class="col">
+      <h5>Section</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+      </ul>
+    </div>
+
+    <div class="col">
+      <h5>Section</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+      </ul>
+    </div>
+
+    <div class="col">
+      <h5>Section</h5>
+      <ul class="nav flex-column">
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Home</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Features</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">Pricing</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">FAQs</a></li>
+        <li class="nav-item mb-2"><a href="#" class="nav-link p-0 text-muted">About</a></li>
+      </ul>
+    </div>
+  </footer>
+</div>
+    <!-- Optional JavaScript; choose one of the two! -->
+
+    <!-- Option 1: Bootstrap Bundle with Popper -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+
+    <!-- Option 2: Separate Popper and Bootstrap JS -->
+    <!--
+    <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" integrity="sha384-7+zCNj/IqJ95wo16oMtfsKbZ9ccEh31eOz1HGyDuCQ6wgnyJNSYdrPa03rtR1zdB" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    -->
+  </body>
+</html>
